bbf235d538d23ddb7ec8d613fe35d088138cdb87
elkallali <r.elkallali@gmail.com>
2024-01-27 15:42:38 -0800

    task completed

--- printf.c
+++ printf.c
@@ -1,5 +1,21 @@
 #include <stdarg.h>
 #include <stdio.h>
+#include "main.h"
+
+/**
+ * _printf: A custom implementation of the printf function that handles the conversion specifiers 'c', 's', and '%'.
+ * @format: The format string, which is composed of zero or more directives.
+ * @...: The variable arguments.
+ *
+ * Returns: The number of characters printed (excluding the null byte used to end output to strings).
+ *
+ * This function handles the following conversion specifiers:
+ * - %c: A character.
+ * - %s: A string.
+ * - %: A percent sign.
+ *
+ * It doesn't handle the flag characters, field width, precision, and length modifiers.
+ */
 
 int _printf(const char *format, ...) {
     int count = 0;
